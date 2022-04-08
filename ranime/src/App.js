02e79ae3bda5de8f31bdfc9396eca877bc5b4a6a
landofcodemonter <mamanlandry@gmail.com>
2022-04-08 08:03:15 +0200

    modif InputZone

--- ranime/src/App.js
+++ ranime/src/App.js
@@ -1,17 +1,12 @@
-import logo from './logo.svg';
-import { Button } from 'react-bootstrap'
 import { Row } from 'react-bootstrap'
-import { Col } from 'react-bootstrap'
-import { Item } from 'react-bootstrap'
 import { ListGroup } from 'react-bootstrap'
 import { Form } from 'react-bootstrap'
-import { Badge } from 'react-bootstrap'
-import { FloatingLabel } from 'react-bootstrap'
 import { Container } from 'react-bootstrap'
 import RListItem from './components/RListItem'
-
-
+import InputZone from './components/InputZone'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Todo from './components/Todo'
+import TodoList from './components/TodoList'
 
 
 function App() {
@@ -23,29 +18,22 @@ function App() {
     <div>
       <Container>
         <Row className="mx-0">
-          <Form>
+          <>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Ma todoYiste</Form.Label>
-              <>
-                <FloatingLabel
-                  controlId="floatingInput"
-                  label="Saisissez une tâche et appuyez 'Entrer'"
-                  className="mb-3"
-                >
-                  <Form.Control type="text" placeholder="name@example.com" />
-                </FloatingLabel>
-              </>
+             <InputZone />
               <Form.Text className="text-muted">
                 <i>Une tâche ne s'entâche que si l'on paresse</i>
               </Form.Text>
             </Form.Group>
-          </Form>
+          </>
         </Row>
         <Row className="mx-0">
           <ListGroup as="ol">
             {listItems}
           </ListGroup>
         </Row>
+        <TodoList />
       </Container> 
     </div>
   );
--- ranime/src/components/InputZone.js
+++ ranime/src/components/InputZone.js
@@ -1,9 +1,45 @@
-import React from 'react'
-
+import React, { useReducer } from 'react'
+import { Form, Button, FloatingLabel } from 'react-bootstrap'
 
 function InputZone() {
+
+
+  const [userInput, setUserInput] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+  {
+    task: ""
+  })
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    console.log(userInput.task)
+    /* const newTodo = { id: uuid(), task: userInput.task, completed: false };
+    createTodo(newTodo);*/
+    setUserInput({ task: "" }); 
+    
+  };
+
+  const handleChange = e => {
+    setUserInput({ [e.target.name] : e.target.value });
+  }
+
   return (
-    <div>InputZone</div>
+    <Form onSubmit={handleSubmit}>
+      <FloatingLabel
+      controlId="floatingInput"
+      label="Saisissez une tâche et appuyez 'Entrer'"
+      className="mb-3"
+    >
+      <Form.Control type="text" 
+        placeholder="task"
+        value={userInput.task} 
+        onChange={handleChange}
+        name="task"/>
+
+    </FloatingLabel>
+
+    <Button type='submit'>Ajouter</Button>
+  </Form>
   )
 }
 
